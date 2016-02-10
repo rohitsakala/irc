155e0477f3c040d670731cea71142d2a6cf7090f
rohitsakala <rohitsakala@gmail.com>
2016-02-10 21:34:32 +0530

    Doen 1

--- index.js
+++ index.js
@@ -5,19 +5,21 @@ var config = {
 };
 
 var irc = require("irc");
+var util = require('util');
+
 var bot = new irc.Client(config.server, config.botName, {
 			channels: config.channels
 });
 
 // Listen for any message, PM said user when he posts
 bot.addListener("message", function(from, to, text, message) {
-			console.log(message);
-			});
+			console.log(util.inspect(message, false, null));
+});
 
 // Listen for any message, say to him/her in the room
 bot.addListener("message", function(from, to, text, message) {
-			console.log(message);
-			});
+			console.log(util.inspect(message, false, null));
+});
 
 
 
