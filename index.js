2adb170782f89dbc8db860ec7ca8dfa8c8762dbd
rohitsakala <rohitsakala@gmail.com>
2016-02-11 01:04:56 +0530

    testing all

--- index.js
+++ index.js
@@ -1,27 +1,59 @@
 var http = require('http');
 var irc = require('irc');
 
-var client = new irc.Client('irc.freenode.net', 'rohitsakalabot', {
+var config = {
+    realName: 'Sakala Venkata Krishna Rohit',
+    port: 6667,
+    localAddress: null,
+    debug: false,
+    showErrors: false,
+    autoRejoin: false,
+    autoConnect: true,
+    channels: [],
+    secure: false,
+    selfSigned: false,
+    certExpired: false,
+    floodProtection: false,
+    floodProtectionDelay: 1000,
+    sasl: true,
+    password: radhaadolf,
+    stripColors: false,
+    channelPrefixes: "&#",
+    messageSplit: 512,
+    encoding: ''
+}
+
+var client = new irc.Client('irc.freenode.net', 'rohitsakala', {
 		  autoConnect: false
 		  });
+
 client.connect(5, function(input) {
 	console.log("Connected!");
 	client.join('#opendaylight', function(input) {
-		console.log("Joined #clojure");
-		client.say('#opendaylight', "Hi");
+		console.log("Joined #opendaylight");
 	});
 });
 
-client.addListener('pm', function (from, text) {
-	console.log("[PM] - " + from + ': ' + text);
-	client.say(from, text);
-	if(text.indexOf('die') !== -1) {
-			client.disconnect("Goodbye, cruel world!", function() {
-			console.log("YOLO");
-		});
+// Listener to be notified when a person joins the channel
+
+client.addListener('message', function (channel, nick, message) {
+	if(nick == "Nithiya")
+	{
+		console.log(channel + '=>' + nick + "has joined IRC");
 	}
 });
+
+// Listener to get messages in the channel
 
 client.addListener('message', function (from, to, text) {
 		  console.log(from + ' => ' + to + ': ' + text);
 });
+
+// Listener to get messages in the channel only of PM
+
+client.addListener('pm', function (from, text) {
+	console.log("[PM] - " + from + ': ' + text);
+	client.say(from, "Leave your message, I will reply when I see the notification to my mobile.");
+});
+
+
