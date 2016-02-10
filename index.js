4aa06fca4107a8b00a424f40145d684b4b62c915
rohitsakala <rohitsakala@gmail.com>
2016-02-10 22:47:09 +0530

    testing

--- index.js
+++ index.js
@@ -6,18 +6,10 @@ var config = {
 	botName: "rohitsakalabot"
 };
 
-var http = require('http');
-http.createServer(function (req, res) {
+var bot = new irc.Client(config.server, config.botName, {
+                        channels: config.channels
+});
 
-		var bot = new irc.Client(config.server, config.botName, {
-			                        channels: config.channels
-			});
-
-		  bot.addListener("message#", function(nick, to, text, message) {
-			      console.log(nick, " :=> ", text);
-			          res.writeHead(200, {'Content-Type': 'text/plain'});
-				      res.write(text);
-				          res.end(text);
-					  });
-		  }).listen(1337, '127.0.0.1');
-console.log('Server running at http://127.0.0.1:1337/');
+bot.addListener("message#", function(nick, to, text, message) {
+		console.log(nick, " :=> ", text);
+});
