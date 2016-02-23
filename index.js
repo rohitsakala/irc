39748f760c3d086ece442e95b9bdb2c6c377de60
rohitsakala <rohitsakala@gmail.com>
2016-02-23 10:54:59 +0530

    Debugging 1

--- index.js
+++ index.js
@@ -3,59 +3,55 @@ var irc = require('irc');
 var port = process.env.PORT || 5000;
 var server = http.createServer();
 server.listen(port);
+
 var config = {
+    userName: 'rohitsakalaBot',
     realName: 'Sakala Venkata Krishna Rohit',
     port: 6667,
     localAddress: null,
     debug: false,
     showErrors: false,
     autoRejoin: false,
-    autoConnect: true,
+    autoConnect: false,
     channels: [],
     secure: false,
     selfSigned: false,
     certExpired: false,
     floodProtection: false,
     floodProtectionDelay: 1000,
-    sasl: true,
-    password: 'radhaadolf',
+    sasl: false,
     stripColors: false,
     channelPrefixes: "&#",
     messageSplit: 512,
     encoding: ''
 }
 
+var client = new irc.Client('irc.freenode.net', 'rohitsakala', config);
 
-var client = new irc.Client('irc.freenode.net', 'rohitsakala');
-client.say("nickserv","identity radhaadolf");
-
-client.connect(5, function(input) {
-	console.log("Connected!");
-	client.join('#opendaylight', function(input) {
-		console.log("Joined #opendaylight");
-	});
+client.connect(5, function(data) {
+    console.log("Connected" + data);
 });
 
 // Listener to be notified when a person joins the channel
 
 //client.addListener('message', function (channel, nick, message) {
-//	if(nick == "Nithiya")
-//	{
-//		console.log(channel + '=>' + nick + "has joined IRC");
-//	}
+//  if(nick == "Nithiya")
+//  {
+//      console.log(channel + '=>' + nick + "has joined IRC");
+//  }
 //});
 
 // Listener to get messages in the channel
 
 //client.addListener('message', function (from, to, text) {
-//		  console.log(from + ' => ' + to + ': ' + text);
+//        console.log(from + ' => ' + to + ': ' + text);
 //});
 
 // Listener to get messages in the channel only of PM
 
 //client.addListener('pm', function (from, text) {
-//	console.log("[PM] - " + from + ': ' + text);
-//	client.say(from, "Leave your message, I will reply when I see the notification on my mobile.");
+//  console.log("[PM] - " + from + ': ' + text);
+//  client.say(from, "Leave your message, I will reply when I see the notification on my mobile.");
 //});
 
 
