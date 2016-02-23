434d25fae4fc0e5bfdb3734d7127eeca0e8d6733
rohitsakala <rohitsakala@gmail.com>
2016-02-23 11:19:12 +0530

    sad

--- index.js
+++ index.js
@@ -34,8 +34,8 @@ client.connect(5, function(data) {
     console.log("Connected  to " + data['server'] );
 });
 
-client.join("#opendaylight",function(data) {
-    console.log(util.inspect(data,false,null));
+client.join('#opendaylight',function(data) {
+    //console.log(util.inspect(data,false,null));
 });
 
 // Listener to be notified when a person joins the channel
