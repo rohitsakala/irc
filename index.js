3e11c20090aecdfa5bdfc79bb4ce10ac876d0ced
rohitsakala <rohitsakala@gmail.com>
2016-02-23 11:53:09 +0530

    sad

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ client.connect(5, function(data) {
 });
 
 client.addListener('pm', function (nick, text, message) {
-    console.log(nick + text + util.inspect(message,false,null));
+    client.say(nick,"Bot :- Logged and sent to Rohit's Personal Mobile.");
 });
 
 //  if(nick == "Nithiya")
